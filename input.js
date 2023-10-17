7117a41d84a8da32cef95cb961901b40df21ec33
Sebastian varon <sebastianvaron96@gmail.com>
2023-10-17 13:30:15 +0000

    sending key movement to the server to move the snake

--- input.js
+++ input.js
@@ -1,16 +1,29 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on('data', handleUserInput);
+  stdin.on("data", handleUserInput);
   return stdin;
 };
 
 const handleUserInput = function (data) {
-  if (data === '\u0003'){
+  if (data === "\u0003") {
     process.exit();
+  } else if (data === "w") {
+    connection.write("Move: up");
+  } else if (data === "a") {
+    connection.write("Move: left");
+  }else if (data === 's') {
+    connection.write('Move: down')
+  }else if (data === 'd') {
+    connection.write('Move: right')
+  } else if (data === 'q'){
+    connection.write('Say: lets go')
   }
-}
+};
 
-module.exports = {setupInput};
+module.exports = { setupInput };
