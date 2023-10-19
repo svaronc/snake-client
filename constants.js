31f77baac85ae6647a8f8bbd9b838561ca8868ab
svaronc <sebastianvaron96@gmail.com>
2023-10-19 21:40:16 +0000

    clean the code and use modules

--- constants.js
+++ constants.js
@@ -5,7 +5,10 @@ const MoveUp = "Move: up";
 const MoveDown = "Move: down";
 const MoveLeft = "Move: left";
 const MoveRight = "Move: right";
-
+const words = {
+  q: "Say: Let's go",
+  e: "Say: i am gonna win",
+};
 module.exports = {
   IP,
   PORT,
@@ -14,4 +17,5 @@ module.exports = {
   MoveDown,
   MoveLeft,
   MoveRight,
+  words,
 };
--- input.js
+++ input.js
@@ -1,5 +1,5 @@
 let connection;
-const { MoveUp, MoveRight, MoveLeft, MoveDown } = require("./constants");
+const { MoveUp, MoveRight, MoveLeft, MoveDown , words} = require("./constants");
 const MoveKeys = {
   w: MoveUp,
   s: MoveDown,
@@ -9,19 +9,21 @@ const MoveKeys = {
 const setupInput = function (conn) {
   connection = conn;
   const stdin = process.stdin;
-  stdin.setRawMode(true);
+  stdin.setRawMode(true); 
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
   return stdin;
 };
 
-// const handleUserInput = function (data) {
-//   if (data === "\u0003") {
-//     process.exit();
-//   } else if (MoveKeys[data]) {
-//     connection.write(MOVEKEYS[data]);
-//   }
-// };
+const handleUserInput = function (data) {
+  if (data === "\u0003") {
+    process.exit();
+  } else if (MoveKeys[data]) {
+    connection.write(MoveKeys[data]);
+  } else if(words[data]){
+    connection.write(words[data])
+  }
+};
 
 module.exports = { setupInput };
